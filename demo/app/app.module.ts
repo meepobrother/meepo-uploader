b375b8294117d183375059a6625c2ec8b1fe2174
15138730370 <1037483576@qq.com>
2017-12-25 09:33:28 +0800

    save to back

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -2,16 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UploaderModule } from '../../src/app/app';
-import { MeepoCoreModule, MeepoCoreServiceModule } from 'meepo-core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    UploaderModule,
-    MeepoCoreModule.forRoot(),
-    MeepoCoreServiceModule
+    UploaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
